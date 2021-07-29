406d34e5e80d1ac55cbbbc0299e1339badf76248
docer <vlasovengineer@gmail.com>
2021-07-29 09:53:24 +0300

    create Controllers create AuthenticationFilter create AuthenticationService create configuration in pom.xml create configuration in web.xml create AuthenticationException

---
+++ src/main/java/taxi/controller/car/GetMyCurrentCarsController.java
@@ -0,0 +1,28 @@
+package taxi.controller.car;
+
+import java.io.IOException;
+import java.util.List;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import taxi.lib.Injector;
+import taxi.model.Car;
+import taxi.service.CarService;
+
+@WebServlet(urlPatterns = "/cars/my")
+public class GetMyCurrentCarsController extends HttpServlet {
+    private static final Injector injector = Injector.getInstance("taxi");
+    private static final String DRIVER_ID = "driver_id";
+    private final CarService carService = (CarService) injector.getInstance(CarService.class);
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
+        Long driverId = (Long) req.getSession().getAttribute(DRIVER_ID);
+        List<Car> allCars = carService.getAllByDriver(driverId);
+        req.setAttribute("cars", allCars);
+        req.getRequestDispatcher("/WEB-INF/views/cars/my.jsp").forward(req, resp);
+    }
+}
